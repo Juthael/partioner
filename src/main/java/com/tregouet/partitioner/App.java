d628adef845d88f056fd7665cb78d56c828fc057
Tregouet <gael.tregouet@gmail.com>
2021-09-18 16:22:25 +0200

    Add files via upload
    initial.

---
+++ src/main/java/com/tregouet/partitioner/App.java
@@ -0,0 +1,13 @@
+package com.tregouet.partitioner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
